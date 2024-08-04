a599574cfd4c32707fed3ca2e8a8bc75fbd51d4f
offic <offic@DESKTOP-GLF4UNU>
2024-08-04 19:55:00 +0600

    Types of Array.

--- src/main/java/com/kamrul/array/Array.java
+++ src/main/java/com/kamrul/array/Array.java
@@ -4,4 +4,21 @@ public class Array {
     public static void main(String[] args) {
         System.out.println("Welcome! Introduction to Array & arrayList.");
     }
+    /*
+    There are 2 types of array.
+        1. one Dimenstional array.       যেমন, int[] number = new int[];
+        2. multi Dimenstional array
+            a. two Dimenstional array    যেমন, int[][] number = new int[][];
+            b. three Dimenstional array  যেমন, int[][][] number = new int[][][];
+    
+    
+    Difference between Array & ArrayList
+        1. Array static হিসেবে কাজ করে, ArrayList dynamic হিসেবে কাজ করে।।
+        2. Array -কে resize করা যায় না, Array -তে size নির্ধারণ করে দিতে হয় ।। 
+           ArrayList -কে resize করা যায়, ArrayList -এ size নির্ধারণ করে দিতে হয় না।। 
+        3. Array -এ (for, for each) loop ব্যবহার করা হয়, ArrayList -এ for each loop, iterator ব্যবহার করা হয়, ।। 
+        4. Array দ্রুত কাজ করে, ArrayList অপেক্ষাকৃত slow কাজ করে।।
+        5. Array -এর ক্ষেত্রে array.length ব্যবহার করা হয়,  ArrayList-এর ক্ষেত্রে array.size ব্যবহার করা হয়,
+
+    */
 }
