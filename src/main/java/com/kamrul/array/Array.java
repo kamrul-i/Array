6c84b31bbc0a96a15da73a8a25083a7975ed0825
offic <offic@DESKTOP-GLF4UNU>
2024-08-04 20:28:13 +0600

    user-এর কাছ থেকে input নিয়ে array যোগ করা হয়েছে।।

--- src/main/java/com/kamrul/array/Array.java
+++ src/main/java/com/kamrul/array/Array.java
@@ -1,36 +1,33 @@
 package com.kamrul.array;
+import java.util.Scanner;
 public class Array {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
         //One Dimenstional array declare
-        int[] number = new int[5];  //5-কে array size বলে, এটা যেকোন সংখ্যা হতে পারে।।
-        //array initialize
-        number[0] = 10;
-        number[1] = 15;
-        number[2] = 20;
-        number[3] = 25;
-        number[4] = 30;
+        double[] number = new double[5]; //5-কে array size বলে, এটা যেকোন সংখ্যা হতে পারে।।
+        double sum = 0;
+        System.out.println("Please Enter any 5 numbers : ");
+        number[0] = input.nextDouble();
+        number[1] = input.nextDouble();
+        number[2] = input.nextDouble();
+        number[3] = input.nextDouble();
+        number[4] = input.nextDouble();
 
-        // array index use
-        int sum = number[0] + number[1] + number[2] + number[3] + number[4];
-        System.out.println("The sum is = " + sum);
-
-        int len = number.length;  //array size show
-        System.out.println("Array size = " + len);
+        sum = number[0] + number[1] + number[2] + number[3] + number[4];
+        System.out.println("The sum is : " + sum);
     }
 
     /*
-        Array size যতো হবে, array initialize ততো হবে।।  
-        array size এর তুলনায় initialize কম হলে সমস্যা হয় না, তবে বেশি initialize করে নিলে সমস্যা হয়।।
-        array size কখনো negative number হয় না।।
+    user-এর কাছ থেকে input নিয়ে array যোগ করা হয়েছে।।
+    
+    Array size যতো হবে, array initialize ততো হবে।।  
+    array size এর তুলনায় initialize কম হলে, সমস্যা হয় না, তবে বেশি initialize করে নিলে সমস্যা হয়।।
+    array size কখনো negative number হয় না।।
+    
+    user-এর কাছ থেকে যতো input নিবেন, input গুলো receive করার জন্য ততোবার initialize করতে হবে।। 
+    এটা অনেক ঝামেলা, পরবর্তী commit-এ loop-এর মাধ্যমে সংক্ষেপে initialize করা হয়েছে।।
         
-        বি.দ্র.: array নামে same class create করতে পারবেন না।। যদিও এখানে সমস্যা করছে না, তবে app crush করবে।।
-        
-        1. Array static হিসেবে কাজ করে।।
-        2. Array -কে resize করা যায় না, Array -তে size নির্ধারণ করে দিতে হয় ।। 
-        3. Array -এ (for, for each) loop ব্যবহার করা হয় ।। 
-        4. Array দ্রুত কাজ করে, ArrayList অপেক্ষাকৃত slow কাজ করে।।
-        5. Array -এর ক্ষেত্রে array.length ব্যবহার করা হয় ।।
-        
-        */
+     */
 }
