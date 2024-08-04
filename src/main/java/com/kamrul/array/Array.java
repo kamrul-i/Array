87c4a2fb9f173bb4476ac3dff711c328cd06c658
offic <offic@DESKTOP-GLF4UNU>
2024-08-04 20:58:39 +0600

    Average.

--- src/main/java/com/kamrul/array/Array.java
+++ src/main/java/com/kamrul/array/Array.java
@@ -4,7 +4,7 @@ public class Array {
 
     public static void main(String[] args) {
         try (Scanner input = new Scanner(System.in)) {
-            
+
             //One Dimenstional array declare
             double number[] = new double[5]; //5-কে array size বলে, এটা যেকোন সংখ্যা হতে পারে।।
             double sum = 0;
@@ -16,12 +16,14 @@ public class Array {
             for (int i = 0; i < number.length; i++) {  //----------(2)
                 sum = sum + number[i];
             }
-            System.out.println("The sum is : " + sum);
+            //System.out.println("The sum is : "+sum); //এ line seen করলে যোগফল ও দেখতে পাবে।।
+            double average = sum / number.length;
+            System.out.println("The average is : " + average);
         }
     }
 
     /*
-    user-এর input দ্বারা loop এর মাধ্যমে array যোগ করা হয়েছে।।
+    user-এর input দ্বারা loop এর মাধ্যমে array যোগ করে average বের করা হয়েছে।।
     
     1নং সমীকরণে user-এর দেওয়া input গুলো loop-এর মাধ্যমে করা initialize করা হয়েছে, যা আগের commit-এ ঝামেলার বলা হয়েছিল।।
     ২নং সমীকরণে user input কে যোগ করা হয়েছে, তারপর result দেখানো হয়েছে।। 
