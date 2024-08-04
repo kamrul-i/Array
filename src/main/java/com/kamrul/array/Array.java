2699af179075829ba3d173341cb0a2e6a6276db5
offic <offic@DESKTOP-GLF4UNU>
2024-08-04 20:43:09 +0600

    user-এর input দ্বারা loop এর মাধ্যমে array যোগ করা হয়েছে।।

--- src/main/java/com/kamrul/array/Array.java
+++ src/main/java/com/kamrul/array/Array.java
@@ -3,31 +3,27 @@ import java.util.Scanner;
 public class Array {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)) {
+            
+            //One Dimenstional array declare
+            double number[] = new double[5]; //5-কে array size বলে, এটা যেকোন সংখ্যা হতে পারে।।
+            double sum = 0;
+            System.out.println("Please Enter any 5 numbers : ");
 
-        //One Dimenstional array declare
-        double[] number = new double[5]; //5-কে array size বলে, এটা যেকোন সংখ্যা হতে পারে।।
-        double sum = 0;
-        System.out.println("Please Enter any 5 numbers : ");
-        number[0] = input.nextDouble();
-        number[1] = input.nextDouble();
-        number[2] = input.nextDouble();
-        number[3] = input.nextDouble();
-        number[4] = input.nextDouble();
-
-        sum = number[0] + number[1] + number[2] + number[3] + number[4];
-        System.out.println("The sum is : " + sum);
+            for (int i = 0; i < number.length; i++) {  //----------(1)
+                number[i] = input.nextDouble();
+            }
+            for (int i = 0; i < number.length; i++) {  //----------(2)
+                sum = sum + number[i];
+            }
+            System.out.println("The sum is : " + sum);
+        }
     }
 
     /*
-    user-এর কাছ থেকে input নিয়ে array যোগ করা হয়েছে।।
+    user-এর input দ্বারা loop এর মাধ্যমে array যোগ করা হয়েছে।।
     
-    Array size যতো হবে, array initialize ততো হবে।।  
-    array size এর তুলনায় initialize কম হলে, সমস্যা হয় না, তবে বেশি initialize করে নিলে সমস্যা হয়।।
-    array size কখনো negative number হয় না।।
-    
-    user-এর কাছ থেকে যতো input নিবেন, input গুলো receive করার জন্য ততোবার initialize করতে হবে।। 
-    এটা অনেক ঝামেলা, পরবর্তী commit-এ loop-এর মাধ্যমে সংক্ষেপে initialize করা হয়েছে।।
-        
+    1নং সমীকরণে user-এর দেওয়া input গুলো loop-এর মাধ্যমে করা initialize করা হয়েছে, যা আগের commit-এ ঝামেলার বলা হয়েছিল।।
+    ২নং সমীকরণে user input কে যোগ করা হয়েছে, তারপর result দেখানো হয়েছে।। 
      */
 }
