178833a192ac67b2a3cad1e3e9f35ff3699b7c18
offic <offic@DESKTOP-GLF4UNU>
2024-08-04 20:17:42 +0600

    Array declare.

--- src/main/java/com/kamrul/array/Array.java
+++ src/main/java/com/kamrul/array/Array.java
@@ -2,23 +2,35 @@ package com.kamrul.array;
 public class Array {
 
     public static void main(String[] args) {
-        System.out.println("Welcome! Introduction to Array & arrayList.");
-    }
-    /*
-    There are 2 types of array.
-        1. one Dimenstional array.       যেমন, int[] number = new int[];
-        2. multi Dimenstional array
-            a. two Dimenstional array    যেমন, int[][] number = new int[][];
-            b. three Dimenstional array  যেমন, int[][][] number = new int[][][];
-    
-    
-    Difference between Array & ArrayList
-        1. Array static হিসেবে কাজ করে, ArrayList dynamic হিসেবে কাজ করে।।
-        2. Array -কে resize করা যায় না, Array -তে size নির্ধারণ করে দিতে হয় ।। 
-           ArrayList -কে resize করা যায়, ArrayList -এ size নির্ধারণ করে দিতে হয় না।। 
-        3. Array -এ (for, for each) loop ব্যবহার করা হয়, ArrayList -এ for each loop, iterator ব্যবহার করা হয়, ।। 
-        4. Array দ্রুত কাজ করে, ArrayList অপেক্ষাকৃত slow কাজ করে।।
-        5. Array -এর ক্ষেত্রে array.length ব্যবহার করা হয়,  ArrayList-এর ক্ষেত্রে array.size ব্যবহার করা হয়,
+        //One Dimenstional array declare
+        int[] number = new int[5];  //5-কে array size বলে, এটা যেকোন সংখ্যা হতে পারে।।
+        //array initialize
+        number[0] = 10;
+        number[1] = 15;
+        number[2] = 20;
+        number[3] = 25;
+        number[4] = 30;
 
-    */
+        // array index use
+        int sum = number[0] + number[1] + number[2] + number[3] + number[4];
+        System.out.println("The sum is = " + sum);
+
+        int len = number.length;  //array size show
+        System.out.println("Array size = " + len);
+    }
+
+    /*
+        Array size যতো হবে, array initialize ততো হবে।।  
+        array size এর তুলনায় initialize কম হলে সমস্যা হয় না, তবে বেশি initialize করে নিলে সমস্যা হয়।।
+        array size কখনো negative number হয় না।।
+        
+        বি.দ্র.: array নামে same class create করতে পারবেন না।। যদিও এখানে সমস্যা করছে না, তবে app crush করবে।।
+        
+        1. Array static হিসেবে কাজ করে।।
+        2. Array -কে resize করা যায় না, Array -তে size নির্ধারণ করে দিতে হয় ।। 
+        3. Array -এ (for, for each) loop ব্যবহার করা হয় ।। 
+        4. Array দ্রুত কাজ করে, ArrayList অপেক্ষাকৃত slow কাজ করে।।
+        5. Array -এর ক্ষেত্রে array.length ব্যবহার করা হয় ।।
+        
+        */
 }
