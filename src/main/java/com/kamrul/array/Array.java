fe93a6374d68653002483ff81273e250b6a0b6eb
offic <offic@DESKTOP-GLF4UNU>
2024-08-04 21:20:52 +0600

    Maximum & Minimum number select.

--- src/main/java/com/kamrul/array/Array.java
+++ src/main/java/com/kamrul/array/Array.java
@@ -6,19 +6,25 @@ public class Array {
         try (Scanner input = new Scanner(System.in)) {
 
             //One Dimenstional array declare
-            double number[] = new double[5]; //5-কে array size বলে, এটা যেকোন সংখ্যা হতে পারে।।
+            double number[] = new double[8]; //5-কে array size বলে, এটা যেকোন সংখ্যা হতে পারে।।
             double sum = 0;
-            System.out.println("Please Enter any 5 numbers : ");
+            System.out.println("Please Enter any 8 numbers : ");
 
             for (int i = 0; i < number.length; i++) {  //----------(1)
                 number[i] = input.nextDouble();
             }
+            double max = number[0];
             double min = number[0];
             for (int i = 1; i < number.length; i++) {
+
+                if (max < number[i]) {
+                    max = number[i];
+                }
                 if (min > number[i]) {
                     min = number[i];
                 }
             }
+            System.out.println("Maximum number is : " + max);
             System.out.println("Minimum number is : " + min);
         }
     }
