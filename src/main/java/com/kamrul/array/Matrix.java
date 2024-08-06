691ce5a679668e83947b0d3116b5e210e6b514da
offic <offic@DESKTOP-GLF4UNU>
2024-08-06 19:52:20 +0600

    matrix declare & use tab.

--- src/main/java/com/kamrul/array/Matrix.java
+++ src/main/java/com/kamrul/array/Matrix.java
@@ -2,11 +2,12 @@ package com.kamrul.array;
 import java.util.Scanner;
 public class Matrix {
     public static void main(String[] args) {
-        
-        Scanner input= new Scanner(System.in);    
+
+        Scanner input = new Scanner(System.in);
         //2 Dimenstional array dicleare 
         int[][] A = new int[2][3];
         int[][] B = new int[2][3];
+
         //---------------------------------------------------------------
         //input from user & initialize it.
         System.out.println("Enter element for A matrix :");
@@ -17,42 +18,43 @@ public class Matrix {
             }
         }
         System.out.println();
+
         //input from user & initialize it.
         System.out.println("Enter element for B matrix :");
         for (int row = 0; row < 2; row++) {
             for (int col = 0; col < 3; col++) {
                 System.out.printf("B[%d][%d] = ", row, col); //-----(3)
-                A[row][col] = input.nextInt();
+                B[row][col] = input.nextInt();
             }
         }
         System.out.println();
         //---------------------------------------------------------------
         //print A matrix
-        System.out.println("A = ");
+        System.out.print("A = ");
         for (int row = 0; row < 2; row++) {
             for (int col = 0; col < 3; col++) {
-                System.out.print(" " + A[row][col]); //------(1)
+                System.out.print("\t " + A[row][col]); //------(1)
             }
             System.out.println(); //--------(2)
         }
         System.out.println();
+
         //print B matrix
-        System.out.println("B = ");
+        System.out.print("B = ");
         for (int row = 0; row < 2; row++) {
             for (int col = 0; col < 3; col++) {
-                System.out.print(" " + A[row][col]); //------(1)
+                System.out.print("\t " + B[row][col]); //------(1)
             }
             System.out.println(); //--------(2)
         }
-        System.out.println();
         //---------------------------------------------------------------
     }
     /*
     এখানে multiple array declare করা হয়েছে।।
     
-    ১নং সমীকরণের space না হলে সকল result একসাথে একত্র হয়ে থাকবে, কিছু বুঝার মতো থাকবে না।।
+    ১নং সমীকরণের  '\t'  result কে খুবই সাজানো-গোছানো অবস্থায় দেখতে সাহায্য করবে।।
     2নং সমীকরণের এই nextline ব্যবহার না করলে সকল result এক line-এ দেখাবে।।
     3নং সমীকরণের এই line, প্রথমেই একটি demo দেখাবে, সে কিভাবে input নিতে চায়।।
-
+    
      */
 }
